f6b6fe857a9e07e3573010d85f8585888a231398
Michele <michele.arduini2323@gmail.com>
2022-07-20 12:40:37 +0200

    modifica
    modifica della pagina che modifica le playlist

--- src/Java/emotionalsongs/EmotionalSongs.java
+++ src/Java/emotionalsongs/EmotionalSongs.java
@@ -54,7 +54,8 @@ public class EmotionalSongs extends Application{
         FileFXML_path + "Playlist_AddSongPage.fxml",
         FileFXML_path + "CommentElement.fxml",
         FileFXML_path + "CommentsPage.fxml",
-        FileFXML_path + "EmotionPage.fxml"
+        FileFXML_path + "EmotionPage.fxml",
+        FileFXML_path + "EditPlaylistPage.fxml"
     };
 
 
