b9e2defb10d2b6cae7607e5babf7e6b6d0cd49c9
MattyMar234 <mattymar2002@gmail.com>
2022-08-13 15:07:23 +0200

    emotions page

--- src/Java/Graphic_Interface/EmotionPageController.java
+++ src/Java/Graphic_Interface/EmotionPageController.java
@@ -5,17 +5,68 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import Java.PlayList_Songs.Song;
+import Java.emotionalsongs.Emotion;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EmotionPageController extends Controller implements Initializable{
 
+    @FXML private Button back;
+    
+    @FXML private TableView<Container> emotionTable;
+    @FXML private TableColumn<Container, String> user;
+    @FXML private TableColumn<Container, String> category;
+    @FXML private TableColumn<Container, String> explanation;
+    @FXML private TableColumn<Container, String> score;
+    
     private Song canzoneAssociata;
     private MainPageController mainController;
+    public ObservableList<Container> list = FXCollections.observableArrayList();
 
-    @FXML private Button back;
+
+    public class Container {
+        
+        private Emotion e;
+        private Song s;
+
+        public String name;
+        public String category;
+        public String description;
+        public String score;
+
+        public Container(Emotion e, Song song) {
+            this.e = e;
+            this.s = song;
+
+            this.name = e.getAccountID();
+            this.category = e.getCategory();
+            this.description = e.getExplanation();
+            this.score = Integer.toString(e.getScore());
+        }
+
+        public String getName() {
+            return name;
+        }
+        public String getDescription() {
+            return description;
+        }
+        public String getScore() {
+            return score;
+        }
+        public String getCategory() {
+            return category;
+        }
+        
+    }
+
+    
 
     public EmotionPageController() {
         super();
@@ -28,7 +79,18 @@ public class EmotionPageController extends Controller implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-                
+
+        for(Emotion e : canzoneAssociata.getEmotions()) {
+            list.add(new Container(e, canzoneAssociata));
+        }
+
+        user.setCellValueFactory(new PropertyValueFactory<Container, String>("name"));
+        category.setCellValueFactory(new PropertyValueFactory<Container, String>("category"));
+        explanation.setCellValueFactory(new PropertyValueFactory<Container, String>("description"));     
+        score.setCellValueFactory(new PropertyValueFactory<Container, String>("score"));     
+    
+        emotionTable.setItems(list);
+        emotionTable.setSelectionModel(null);
     }
     
 
