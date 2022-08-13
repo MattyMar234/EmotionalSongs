bbb1222c2441e697ce2384145779e3e3e417b4f5
MattyMar234 <mattymar2002@gmail.com>
2022-08-13 19:03:47 +0200

    sorting

--- src/Java/Graphic_Interface/MainPageController_reposity.java
+++ src/Java/Graphic_Interface/MainPageController_reposity.java
@@ -23,6 +23,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TableCell;
@@ -44,6 +45,43 @@ import javafx.util.Callback;
 public class MainPageController_reposity extends Controller implements Initializable 
 {
 
+    // ========================= variabili =========================//
+    @FXML private TextField KeywordTextField;
+    @FXML private TableView<Container> SongsTable;
+    @FXML private TableColumn<Container, Container> Elements;
+    
+    @FXML private Button CambioButton;
+    @FXML private Button playlistButton;
+    @FXML private Button profileButton;
+    @FXML private Button optionsButton;
+    @FXML private Button ExitButton;
+    @FXML private Label SerachLabel;
+    @FXML private ComboBox<FilterState> filter;
+
+    private ObservableList<Container> list = FXCollections.observableArrayList();
+    public MainPageController mainController;
+    private int counter = 0;
+
+
+    private enum FilterState {
+        
+        ANNO_CRESCENTE, 
+        ANNO_DECRESCENTE, 
+        COMMENTI_CRESCENTE, 
+        COMMENTI_DECRESCENTE, 
+        EMOZIONI_CRESCENTE,
+        EMOZIONI_DECRESCENTE;
+
+        @Override
+        public String toString() {
+            String s = super.toString();
+            s = "Per " + s.toString().toLowerCase().replace("_", " ");
+            
+            return s;
+        }
+    }
+
+
     public class Container {
 
         public Song song;
@@ -58,33 +96,8 @@ public class MainPageController_reposity extends Controller implements Initializ
 
         public Container getClassReference() {
             return this;
-        }
-        
-        
+        } 
     }
-
-    // ========================= tabelle =========================//
-    @FXML private TextField KeywordTextField;
-    @FXML private TableView<Container> SongsTable;
-    @FXML private TableColumn<Container, Container> Elements;
-
-    // ========================= Label ========================= //
-    @FXML private Label SerachLabel;
-
-   
-    // ========================= Buttons ========================= //
-    @FXML private Button CambioButton;
-    @FXML private Button playlistButton;
-    @FXML private Button profileButton;
-    @FXML private Button optionsButton;
-    @FXML private Button ExitButton;
-
-    private int counter = 0;
-
-
-    // ========================= variabili =========================//
-    private ObservableList<Container> list = FXCollections.observableArrayList();
-    public MainPageController mainController;
 
 
     // ============================================================ //
@@ -95,13 +108,11 @@ public class MainPageController_reposity extends Controller implements Initializ
 
     @FXML
     void scroll(ScrollEvent event) {
-        System.out.println("here");
-        System.out.println(event.getDeltaX());
+        /*System.out.println("here");
+        System.out.println(event.getDeltaX());*/
     }
 
     
-
-
 
 
     @Override
@@ -110,6 +121,14 @@ public class MainPageController_reposity extends Controller implements Initializ
         for(Song song : application.songManager.getList()) {
             list.add(new Container(song, this));
         }
+
+
+        /*for(FilterState e : FilterState.values()) {
+            //filter.getItems().add("Per " + e.toString().toLowerCase().replace("_", " "));  
+        }*/
+
+        filter.getItems().addAll(FilterState.values());
+        filter.setValue(filter.getItems().get(0));
 
         //ScrollBar verticalBar = (ScrollBar) SongsTable.lookup(".scroll-bar:vertical");
         //verticalBar.setValue(0.49);
@@ -231,6 +250,96 @@ public class MainPageController_reposity extends Controller implements Initializ
     public void Selezione(ActionEvent event) {
         System.out.println("pressed");
 
+    }
+
+    private int sortGetData(FilterState F, Song S) {
+        switch (F) 
+        {
+            case ANNO_CRESCENTE, ANNO_DECRESCENTE:
+                return Integer.parseInt(S.getYear());
+
+            case COMMENTI_DECRESCENTE, COMMENTI_CRESCENTE:
+                return S.getComments().size();
+      
+
+            case EMOZIONI_CRESCENTE, EMOZIONI_DECRESCENTE:
+                return S.getEmotions().size();
+
+            default: return 0;
+        }
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+        
+        FilterState state = filter.getValue();
+        System.out.println(state);
+
+        int N = list.size();
+        int min = sortGetData(state, list.get(0).song);
+        int max = sortGetData(state, list.get(0).song);
+        int range;
+        int count[];
+
+        Container a[] = new Container[list.size()];
+
+        int counter = 0;
+        for(Container c : list) {
+            a[counter++] = c;
+        }
+
+        for(Container c : a) 
+        {
+            int v = sortGetData(state, c.song);
+            
+            if(v <  min) {
+                min = v;
+            }
+            else if(v > max) {
+                max = v;
+            }
+        }
+
+        range = max - min + 1;
+
+        count = new int[range];
+        for(int i = 0; i < range; i++) count[i] = 0;
+
+        //freq aasolute
+        for(int i = 0; i < N; i++) count[sortGetData(state, a[i].song) - min]++;
+        
+        //freq culumalte
+        for(int i = 1; i < range; i++) count[i] += count[i - 1];
+
+
+        Container [] b = new Container[N];
+        
+
+        if (state == FilterState.ANNO_CRESCENTE || state == FilterState.COMMENTI_CRESCENTE || state == FilterState.EMOZIONI_CRESCENTE) {
+            for(int i = 0; i < N; i++) {
+                int freq = count[sortGetData(state, a[i].song) - min]--;
+                b[N - (freq - 1) - 1] = a[i];
+            }
+        }
+        else {
+            for(int i = 0; i < N; i++) {
+                int freq = count[sortGetData(state, a[i].song) - min]--;
+                b[(freq - 1)] = a[i];
+            }
+        }
+
+        
+
+        list.clear();
+        list.addAll(b);
+
+        
+
+
+
+        
+
+        
     }
 
 }
